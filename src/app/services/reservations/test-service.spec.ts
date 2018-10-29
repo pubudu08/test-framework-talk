cbbbb00b7d4107b2cfa02ceefa3b2ac0dfc36470
Pubudu Dissanayake <pubudu08@gmail.com>
2018-10-29 21:56:39 +1100

    initial tdd

--- src/app/services/reservations/test-service.spec.ts
+++ src/app/services/reservations/test-service.spec.ts
@@ -1,4 +1,12 @@
-describe('operations', () => {
-  it('happy path', async () => {
+import * as calculator from  './test-service'
+
+describe('Arithmetic Operations', () => {
+
+  it('Should return 0 if empty string provided',  () => {
+    expect(calculator.add('')).toBe(0)
+  })
+
+  it('Should return single provided element',  () => {
+    expect(calculator.add('1')).toBe(1)
   })
 })
