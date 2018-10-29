c3c4be6525b0eb139d492c07a3b702bc8afccba4
Pubudu Dissanayake <pubudu08@gmail.com>
2018-10-29 22:32:53 +1100

    v2.1

--- src/app/services/calculator/test-service.ts
+++ src/app/services/calculator/test-service.ts
@@ -1,9 +1,21 @@
-
 import * as _ from 'lodash'
 
 export const add = (value: string): number => {
-  if (_.isEmpty(value)){
+
+  if (_.isEmpty(value)) {
     return 0
   }
-  return parseInt(value)
+  if (value.length === 1) {
+    return parseInt(value)
+  }
+  const items = _.split(value, ',')
+
+  let sum = 0
+
+  items.map((item) => {
+    sum += parseInt(item)
+  })
+
+  return sum
+
 }
